12b677af8c526c507321f8ddc26ebb872a69556c
Nikhil#Devmaanush <93393503+devmanush-forEach@users.noreply.github.com>
2021-12-16 13:17:29 +0530

    Add files via upload

---
+++ javascript/guides.js
@@ -0,0 +1,41 @@
+import data from "/javascript/guidesData.js";
+showGuides();
+function showGuides() {
+    let guide_list = document.getElementById("guide_list");
+    data.map((el) => {
+        let guide_item = document.createElement("li");
+        guide_item.className = "guide_items"
+
+
+        let img_link = document.createElement("a");
+        img_link.src = "#";
+
+
+        let img = document.createElement("img");
+        img.className = "guide_img";
+        img.src = el.img;
+
+        img_link.append(img);
+
+        let book_link = document.createElement("div");
+        book_link.className = "book_link"
+
+        let book_a = document.createElement("a");
+        book_a.href = "#"
+        book_a.textContent = "Read Online >"
+        book_link.append(book_a)
+
+
+        let git_link = document.createElement("div");
+        git_link.className = "git_link"
+
+        let git_a = document.createElement("a")
+        git_a.href = el.git;
+        git_a.textContent = "Github Repo >"
+        git_link.append(git_a);
+
+        guide_item.append(img_link, book_link, git_link);
+        guide_list.appendChild(guide_item);
+
+    })
+}
---
+++ javascript/guidesData.js
@@ -0,0 +1,41 @@
+
+let data = [
+
+    {
+        img: "https://frontendmasters.com/static-assets/guides/learning-roadmap.jpg",
+        git : "https://github.com/FrontendMasters/learning-roadmap"
+    },
+    {
+        
+        img: "https://frontendmasters.com/static-assets/guides/javascript-enlightenment.jpg",
+        git : "https://github.com/FrontendMasters/javascript-enlightenment"
+    },
+    {
+        img: "https://frontendmasters.com/static-assets/guides/front-end-handbook-2019.jpg",
+        git : "https://github.com/FrontendMasters/front-end-handbook-2019"
+        
+    },
+    {
+        img: "https://frontendmasters.com/static-assets/guides/front-end-handbook-2018.jpg",
+        git : "https://github.com/FrontendMasters/front-end-handbook-2018"
+        
+    },
+    {
+        img: "https://frontendmasters.com/static-assets/guides/front-end-handbook-2017.jpg",
+        git : "https://github.com/FrontendMasters/front-end-handbook-2017"
+        
+    },
+    {
+        
+        img: "https://frontendmasters.com/static-assets/guides/front-end-handbook-2016.jpg",
+        git : "https://github.com/FrontendMasters/front-end-handbook"
+    },
+    {
+        img: "https://frontendmasters.com/static-assets/guides/react-enlightenment.jpg",
+        git : "https://github.com/FrontendMasters/react-enlightenment"
+
+    },
+
+]
+
+export default data;
