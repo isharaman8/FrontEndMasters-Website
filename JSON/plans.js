a14d8a11e953ec1a694c097d149ff63488944992
Aman Kumar <isharaman8@gmail.com>
2021-12-18 10:28:52 +0530

    created payments page

--- JSON/plans.js
+++ JSON/plans.js
@@ -1,8 +1,9 @@
 class Premiums {
-	constructor(planType, planPrice, description) {
+	constructor(planType, planPrice, description, id) {
 		this.planType = planType;
 		this.planPrice = planPrice;
 		this.description = description;
+		this.id = id;
 	}
 }
 
@@ -11,28 +12,32 @@ const plans = [
 		...new Premiums(
 			"MONTHLY",
 			"$39",
-			"Access all premium courses, workshops, and mobile apps. Renewed monthly."
+			"Access all premium courses, workshops, and mobile apps. Renewed monthly.",
+			"monthly"
 		),
 	},
 	{
 		...new Premiums(
 			"YEARLY",
 			"$390",
-			`Access everything. Renewed yearly. Save $78 per year!`
+			`Access everything. Renewed yearly. Save $78 per year!`,
+			"yearly"
 		),
 	},
 	{
 		...new Premiums(
 			"MONTHLY TEAM",
 			"$195",
-			"Access for 10 people at your company, renewed monthly. Save 50% on our monthly plans!"
+			"Access for 10 people at your company, renewed monthly. Save 50% on our monthly plans!",
+			"monthly_t"
 		),
 	},
 	{
 		...new Premiums(
 			"YEARLY TEAM",
 			"$1950",
-			"Access for 10 people at your company, renewed yearly. Save 50% on our yearly plans!"
+			"Access for 10 people at your company, renewed yearly. Save 50% on our yearly plans!",
+			"yearly_t"
 		),
 	},
 ];
--- LOGIN_SIGNUP/js/signup.js
+++ LOGIN_SIGNUP/js/signup.js
@@ -1,21 +1,21 @@
 import plans from "../../JSON/plans.js";
-console.log(plans);
 document.querySelector("#register").addEventListener("submit", registerUser);
 
 function mapPlans(plans) {
 	plans.forEach((plan) => {
-		console.log(plan);
 		// CREATING ELEMENTS
 		const planName = document.createElement("h3");
 		const planDesc = document.createElement("p");
 		const price = document.createElement("p");
 		const tickIcon = document.createElement("i");
+		const radioButton = document.createElement("input");
 
 		// spans
 		const priceSpan = document.createElement("span");
 
 		// divs
-		const textDiv = document.createElement("div");
+		const textLabel = document.createElement("label");
+		const mainDiv = document.createElement("div");
 
 		// SETTING ATTRIBUTES AND TEXT CONTENTS
 
@@ -23,30 +23,50 @@ function mapPlans(plans) {
 		planDesc.textContent = plan.description;
 		tickIcon.innerHTML = `check_circle`;
 		price.textContent = plan.planPrice;
+		radioButton.setAttribute("type", "radio");
 
 		// Add Classes and ids
 		tickIcon.classList.add("material-icons");
 		tickIcon.classList.add("tickIcon");
-		textDiv.classList.add("planTextDiv");
+		textLabel.classList.add("planTextDiv");
+		mainDiv.classList.add("planMainDiv");
 		priceSpan.classList.add("priceSpan");
+		radioButton.name = "allPlans";
+		radioButton.classList.add("planRadioButton");
+		radioButton.id = plan.id;
+		textLabel.setAttribute("for", plan.id);
 
 		// appending
 		priceSpan.append(tickIcon, price);
-		textDiv.append(planName, priceSpan, planDesc);
+		textLabel.append(planName, priceSpan, planDesc);
+		mainDiv.append(radioButton, textLabel);
 
-		document.querySelector("#planSelect").append(textDiv);
+		document.querySelector("#planSelect").append(mainDiv);
 	});
 }
 
 async function registerUser(event) {
 	event.preventDefault();
 
+	// FOR CHECKED PLAN
+
 	let name = `${document.querySelector("#firstName").value} ${
 		document.querySelector("#lastName").value
 	}`;
-	let description = `Payment Method Selected: ${
-		document.querySelector("#paymentsSelect").value
-	}`;
+	let description = [
+		document.querySelector("#cardNumber").value,
+		document.querySelector("#expiryDate").value,
+		document.querySelector("#cvc").value,
+	].join(",");
+
+	const plansNodes = document.querySelectorAll(`input[name="allPlans"]`);
+	let checkedPlan;
+	for (let i = 0; i < plansNodes.length; i++) {
+		if (plansNodes[i].checked) {
+			checkedPlan = plansNodes[i].id;
+			break;
+		}
+	}
 
 	let paymentMode = document.querySelector("#paymentsSelect").value;
 	const userObj = {
@@ -81,14 +101,23 @@ async function registerUser(event) {
 		},
 	});
 	const data = await api.json();
-	checkValidUser(data, paymentMode);
+	checkValidUser(userObj, data, paymentMode, checkedPlan);
 }
 
-function checkValidUser(data, paymentMode) {
+function checkValidUser(userObj, data, paymentMode, checkedPlan) {
+	// Creating Plan Object
+	const planObj = {
+		monthly: "39",
+		yearly: "390",
+		monthly_t: "195",
+		yearly_t: "1950",
+	};
+
 	if (data.error == false) {
 		alert("Registration Successful");
-		localStorage.setItem("frontEndUser", JSON.stringify(data));
-
+		const userPlan = [checkedPlan, planObj[checkedPlan]];
+		localStorage.setItem("frontEndUserPlan", JSON.stringify(userPlan));
+		localStorage.setItem("frontEndUser", JSON.stringify(userObj));
 		if (paymentMode === "cdCard") window.location.href = "cdCard.html";
 		else window.location.href = "payPal.html";
 	} else {
@@ -96,5 +125,12 @@ function checkValidUser(data, paymentMode) {
 	}
 }
 
+// Defining css
+
 // initializing functions
 mapPlans(plans);
+
+document.addEventListener("DOMContentLoaded", function () {
+	const allPlans = document.querySelectorAll(`input[name="allPlans"]`);
+	allPlans[0].checked = true;
+});
